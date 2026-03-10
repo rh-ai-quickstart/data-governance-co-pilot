26f69cfb3049d59f27e0f2e20628d9767878b77a
Peter Samouelian <psamouel@redhat.com>
2026-03-10 14:52:50 -0400

    Optimize data loader deployment.

--- build_and_push.sh
+++ build_and_push.sh
@@ -13,3 +13,7 @@ podman push quay.io/rh-ai-quickstart/pg-airman-mcp:latest
 cd ../../
 podman build --platform linux/amd64 -t quay.io/rh-ai-quickstart/copilot-backend:latest -f helm/copilot-backend/Containerfile .
 podman push quay.io/rh-ai-quickstart/copilot-backend:latest
+
+cd helm/pgvector/
+podman build --platform linux/amd64 -t quay.io/rh-ai-quickstart/pgvector-data-loader:latest -f Containerfile .
+podman push quay.io/rh-ai-quickstart/pgvector-data-loader:latest
