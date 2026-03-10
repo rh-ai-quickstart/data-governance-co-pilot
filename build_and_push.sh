06e8c609d5151ea8ccab22a466d1da210e98be8b
Peter Samouelian <psamouel@redhat.com>
2026-03-10 14:06:16 -0400

    Optimized MCP server deployment.

--- build_and_push.sh
+++ build_and_push.sh
@@ -6,3 +6,6 @@ cd ./apps/ui/
 podman build --platform linux/amd64 -t quay.io/rh-ai-quickstart/copilot-ui:latest -f Containerfile .
 podman push quay.io/rh-ai-quickstart/copilot-ui:latest
 
+cd ../../helm/pg-airman-mcp/
+podman build --platform linux/amd64 -t quay.io/rh-ai-quickstart/pg-airman-mcp:latest -f Containerfile .
+podman push quay.io/rh-ai-quickstart/pg-airman-mcp:latest
